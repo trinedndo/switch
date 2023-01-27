123bdf402601e9ba3aa5a247948e9824ada95113
Name <shimaguds@gmail.com>
2023-01-27 08:33:36 +0300

    fff

---
+++ src/App.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import router from "./router.js";
+import cookieParser from 'cookie-parser';
+
+const PORT = process.env.PORT || 8000;
+const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}))
+
+// app.use(express.session({
+//     secret: conf.secret,
+//     cookie: { domain: '.yourdomain.com' },
+//     store: new MongoStore(conf.sessiondb)
+// }));
+app.use("/api", router);
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT: ${PORT}`);
+        })
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+start()
