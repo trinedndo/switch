000445f473c9d62941d4c396700738112de919f7
Name <shimaguds@gmail.com>
2023-01-27 08:44:24 +0300

    asdkflj

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import router from "./router.js";
 import cookieParser from 'cookie-parser';
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(express.json());
